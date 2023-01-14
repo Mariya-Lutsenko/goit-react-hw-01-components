71d38a1cab6eba28994565cef1b2b1f8646aadf8
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-01-15 01:08:01 +0200

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import { Container } from './Container/Container';
 import user from '../configs/user';
 import { Profile } from './Profile/Profile';
 
+import data from '../configs/data';
+import { Statistics } from './Statistics/Statistics';
+
 export const App = () => {
   return (
     <Container>
@@ -13,6 +16,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </Container>
   );
 };
