f1d5d577e7b3aa8bc4fe6c9457d0c04aea912a99
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-01-14 22:51:57 +0200

    add  Container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-import  user from '../configs/user' ;
-import {Profile } from "./Profile/Profile";
+import { Container } from './Container/Container';
 
+import user from '../configs/user';
+import { Profile } from './Profile/Profile';
 
 export const App = () => {
   return (
-    <div>
-      <Profile 
+    <Container>
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
-        stats={user.stats}/>
-    </div>
+        stats={user.stats}
+      />
+    </Container>
   );
 };
